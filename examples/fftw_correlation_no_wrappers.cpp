bac42a523d63b29402f0357ede167d918696d1aa
Carlos O'Ryan <coryan@users.noreply.github.com>
2015-09-03 08:01:06 -0400

    Add comments to explain what is going on in the example

--- examples/fftw_correlation_no_wrappers.cpp
+++ examples/fftw_correlation_no_wrappers.cpp
@@ -11,6 +11,7 @@ int main(int argc, char* argv[]) try {
   int size = 2048;
   int delay = size / 8;
 
+  // ... create a step function ...
   std::vector<std::complex<double>> a(size);
   std::vector<std::complex<double>> b(size);
   for (int i = 0; i < size / 4; ++i) {
@@ -23,6 +24,7 @@ int main(int argc, char* argv[]) try {
     a[i] = std::complex<double>(1, 0);
   }
 
+  // ... and then create a delayed version of the same step function ...
   for (int i = 0; i < delay; ++i) {
     b[i] = a.at(size - size/8 + i);
   }
@@ -30,32 +32,39 @@ int main(int argc, char* argv[]) try {
     b[i] = a[i - delay];
   }
 
+  // ... create vectors to receive the results of each FFT ...
   std::vector<std::complex<double>> ta(a.size());
   std::vector<std::complex<double>> tb(a.size());
+
+  // ... FFTW stores plans of the FFT transform ...
   fftw_plan ffta = fftw_plan_dft_1d(
       a.size(), reinterpret_cast<fftw_complex*>(&a[0]),
       reinterpret_cast<fftw_complex*>(&ta[0]), FFTW_FORWARD, FFTW_ESTIMATE);
   fftw_plan fftb = fftw_plan_dft_1d(
       b.size(), reinterpret_cast<fftw_complex*>(&b[0]),
       reinterpret_cast<fftw_complex*>(&tb[0]), FFTW_FORWARD, FFTW_ESTIMATE);
+
+  // ... create a vector to store the result of Conj(ta) * tb ...
   std::vector<std::complex<double>> prod(size);
+  // ... create a vector to stored FFT^1(prod) ...
   std::vector<std::complex<double>> correlation(a.size());
+  // ... plan the inverse FFT ...
   fftw_plan inv = fftw_plan_dft_1d(
       a.size(), reinterpret_cast<fftw_complex*>(&prod[0]),
       reinterpret_cast<fftw_complex*>(&correlation[0]), FFTW_BACKWARD,
       FFTW_ESTIMATE);
 
   auto start = std::chrono::steady_clock::now();
+  // Compute FFT(a) and FFT(b) ...
   fftw_execute(ffta);
   fftw_execute(fftb);
-
+  // ... compute Conj(FFT(a)) * FFT(b) ...
   for (std::size_t i = 0; i != ta.size(); ++i) {
     prod[i] = std::conj(ta[i]) * tb[i];
   }
-
-
+  // ... compute FFT^1(Conj(FFT(a)) * FFT(b) ...
   fftw_execute(inv);
-
+  // ... find max and argmax in FFT^1(Conj(FFT(a)) * FFT(b) ...
   std::size_t argmax = 0;
   double max = std::numeric_limits<double>::min();
   for (std::size_t i = 0; i != correlation.size(); ++i) {
@@ -65,17 +74,19 @@ int main(int argc, char* argv[]) try {
     }
   }
   auto end = std::chrono::steady_clock::now();
+
+  // ... cleanup the plans ...
   fftw_destroy_plan(inv);
   fftw_destroy_plan(fftb);
   fftw_destroy_plan(ffta);
 
+  // ... produce some output, the timing should not be taken too
+  // seriously ...
+  auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(
+      end - start);
   std::cout << "delay=" << delay << ", argmax=" << argmax
             << ", max=" << max << std::endl
-            << "elapsed="
-            << std::chrono::duration_cast<std::chrono::microseconds>(
-                end - start).count()
-            << std::endl;
-  
+            << "elapsed=" << elapsed.count() << std::endl;
 
   return 0;
 } catch(std::exception const& ex) {
