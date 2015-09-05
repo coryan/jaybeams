a5717cb37e45bc2663cba7212576be894845f623
Carlos O'Ryan <coryan@users.noreply.github.com>
2015-09-04 22:57:20 -0400

    Add allocator based on fftw*_malloc()

---
+++ jb/fftw/allocator.hpp
@@ -0,0 +1,52 @@
+#ifndef jb_fftw_allocator_hpp
+#define jb_fftw_allocator_hpp
+
+#include <fftw3.h>
+#include <limits>
+#include <memory>
+
+namespace jb {
+namespace fftw {
+
+/**
+ * Define an allocator based on fftw_malloc()/fftw_free()
+ *
+ * FFTW3 provides functions to allocate memory aligned to whatever
+ * requirements the vectorized instructions require.
+ */
+template<typename T>
+class allocator {
+ public:
+  typedef T value_type;
+  typedef value_type* pointer;
+  typedef value_type const* const_pointer;
+  typedef void* void_pointer;
+  typedef void const* const_void_pointer;
+  typedef std::size_t size_type;
+  typedef std::ptrdiff_t difference_type;
+  template<typename U> struct rebind { typedef allocator<U> other; };
+
+  pointer address(T& object) const { return &object; }
+  const_pointer address(T const& object) const { return &object; }
+  size_type max_size() const {
+    return std::numeric_limits<std::size_t>::max();
+  }
+  template<typename... Args>
+  void construct(pointer p, Args&&... args) {
+    new (static_cast<void*>(p)) T(std::forward<Args>(args)...);
+  }
+  void destroy(pointer p) {
+    p->~T();
+  }
+  pointer allocate(size_type count, const void* = 0) {
+    return reinterpret_cast<T*>(fftw_malloc(count * sizeof(T)));
+  }
+  void deallocate(pointer p, size_type count) {
+    fftw_free(static_cast<void*>(p));
+  }
+};
+
+} // namespace fftw
+} // namespace jb
+
+#endif // jb_fftw_allocator_hpp
---
+++ jb/fftw/ut_allocator.cpp
@@ -0,0 +1,65 @@
+#include <jb/fftw/allocator.hpp>
+#include <jb/fftw/plan.hpp>
+#include <jb/testing/check_vector_close_enough.hpp>
+
+#include <boost/test/unit_test.hpp>
+#include <vector>
+
+namespace {
+
+template<typename T> using aligned_vector =
+    std::vector<T, jb::fftw::allocator<T>>;
+   
+
+template<typename precision_t>
+void test_plan_real2complex() {
+  int nsamples = 1<<15;
+  int tol = nsamples;
+
+  typedef jb::fftw::plan<precision_t> tested;
+  typedef typename tested::precision_type precision_type;
+  typedef std::complex<precision_type> complex;
+
+  aligned_vector<precision_t> in(nsamples);
+  aligned_vector<complex> tmp(nsamples);
+  aligned_vector<precision_t> out(nsamples);
+  
+  std::size_t h = nsamples / 2;
+  for (std::size_t i = 0; i != h; ++i) {
+    in[i] = i - h/4.0;
+    in[i + h] = h/4.0 - i;
+  }
+  
+  tested dir = tested::create_forward(in, tmp);
+  tested inv = tested::create_backward(tmp, out);
+
+  dir.execute(in, tmp);
+  inv.execute(tmp, out);
+  for (std::size_t i = 0; i != std::size_t(nsamples); ++i) {
+    out[i] /= nsamples;
+  }
+  jb::testing::check_vector_close_enough(out, in, tol);
+}
+
+} // anonymous namespace
+
+/**
+ * @test Verify that we can use jb::fftw::allocator<>
+ */
+BOOST_AUTO_TEST_CASE(fftw_allocator_double) {
+  test_plan_real2complex<double>();
+}
+
+/**
+ * @test Verify that we can use jb::fftw::allocator<>
+ */
+BOOST_AUTO_TEST_CASE(fftw_allocator_float) {
+  test_plan_real2complex<float>();
+}
+
+/**
+ * @test Verify that we can use jb::fftw::allocator<>
+ */
+BOOST_AUTO_TEST_CASE(fftw_allocator_long_double) {
+  test_plan_real2complex<long double>();
+}
