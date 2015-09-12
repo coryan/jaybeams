634685b08beee00b4efc6b9a3f28d78544f96637
Carlos O'Ryan <coryan@users.noreply.github.com>
2015-09-12 19:40:38 -0400

    Read the original governor and save it at the end

--- tools/benchmark_fftw_time_delay_estimator.sh
+++ tools/benchmark_fftw_time_delay_estimator.sh
@@ -3,18 +3,24 @@
 # ... exit on the first error, print out what commands we will run ...
 set -ev
 
+# ... get the current CPU power management settings ...
+governor=`cpupower frequency-info -p | cut -f3,3 -d' ' | grep -v :`
+
 # ... set the CPU power management to optimize for performance ...
 sudo cpupower frequency-set -g performance
 
+# ... print out the CPU power settings in case they did not have the
+# intended effect ...
+sudo cpupower frequency-info
+
 # ... make sure we reset the 
-trap "sudo cpupower frequency-set -g ondemand" 0
+trap "sudo cpupower frequency-set -g $governor" 0
 
 # ... run different iterations of the benchmark ...
 for test in double:aligned float:aligned double:unaligned float:unaligned; do
     echo ---
     ./jb/fftw/bm_time_delay_estimator --test-case=${test} --iterations=1000000
 done
-
 
 # ... print out compilation details ...
 cat jb/testing/compile_info.cpp
