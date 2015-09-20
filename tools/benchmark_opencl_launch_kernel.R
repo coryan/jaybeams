e150a005536a687a399239a496afdd59898c16ac
Carlos O'Ryan <coryan@users.noreply.github.com>
2015-09-20 17:11:41 -0400

    Generate linear fit in the R script, and PNG files from the R script, to include in github issues.

--- tools/benchmark_opencl_launch_kernel.R
+++ tools/benchmark_opencl_launch_kernel.R
@@ -10,7 +10,10 @@ data <- read.csv(
   comment.char='#')
 
 # compute the cost per OpenCL kernel launch
-data$cost <- data$nanoseconds / data$size
+data$cost <- data$nanoseconds / data$size / 1000.0
+
+# express the time in microseconds
+data$microseconds <- data$nanoseconds / 1000.0
 
 # turn the size (number of OpenCL kernel launches) into a factor so
 # each gets a different error bar.
@@ -18,11 +21,30 @@ data$fsize <- factor(data$size)
 
 # plot the data
 ggplot(data=data, aes(x=fsize, y=cost)) + geom_boxplot() +
-  ylab("nanoseconds per kernel launch") +
-  xlab("number of kernels launched (pipelined)")
+  ylab("Microseconds per kernel launch (log scale)") +
+  xlab("Number of kernels launched (pipelined)") +
+  scale_y_log10()
 
 # save the plot
-ggsave(filename="benchmark_opencl_launch_kernel.svg",
-       width=8.0, height=8.0/1.61);
+ggsave(filename="benchmark_opencl_launch_kernel.boxplot.svg",
+       width=8.0, height=8.0/1.61)
+ggsave(filename="benchmark_opencl_launch_kernel.boxplot.png",
+       width=8.0, height=8.0/1.61)
+
+# plot the raw data with a regression
+ggplot(data=data, aes(x=size, y=microseconds)) +
+  geom_point(alpha=0.05) +
+  geom_smooth(method='lm') +
+  ylab("Microeconds to launch all kernels") +
+  xlab("Number of kernels launched (pipelined)")
+ggsave(filename="benchmark_opencl_launch_kernel.fit.svg",
+       width=8.0, height=8.0/1.61)
+ggsave(filename="benchmark_opencl_launch_kernel.fit.png",
+       width=8.0, height=8.0/1.61)
+
+# Fit the data using a linear regression
+fit <- lm(nanoseconds ~ size, data)
+summary(fit)
+print(fit)
 
 q(save="no")
