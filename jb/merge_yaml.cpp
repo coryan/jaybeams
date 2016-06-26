78b6129640589ee3478377433a77df58a7c97d23
Carlos O'Ryan <coryan@users.noreply.github.com>
2016-06-26 14:50:02 -0400

    Refactor functions to merge YAML objects.  This way it is easier to test them

---
+++ jb/merge_yaml.cpp
@@ -0,0 +1,78 @@
+#include <jb/merge_yaml.hpp>
+#include <jb/assert_throw.hpp>
+
+void jb::yaml::merge_node(
+    YAML::Node target, YAML::Node const& source) {
+  switch(source.Type()) {
+    case YAML::NodeType::Scalar:
+      target = source.Scalar();
+      break;
+    case YAML::NodeType::Map:
+      merge_map(target, source);
+      break;
+    case YAML::NodeType::Sequence:
+      merge_sequences(target, source);
+      break;
+    case YAML::NodeType::Null:
+      throw std::runtime_error(
+          "merge_node: Null source nodes not supported");
+    case YAML::NodeType::Undefined:
+      throw std::runtime_error(
+          "merge_node: Undefined source nodes not supported");
+  }
+}
+
+void jb::yaml::merge_map(
+    YAML::Node target, YAML::Node const& source) {
+  for (auto const& j : source) {
+    merge_node(target[j.first.Scalar()], j.second);
+  }
+}
+
+void jb::yaml::merge_sequences(
+    YAML::Node target, YAML::Node const& source) {
+  for (std::size_t i = 0; i != source.size(); ++i) {
+    if (i < target.size()) {
+      merge_node(target[i], source[i]);
+    } else {
+      target.push_back(YAML::Clone(source[i]));
+    }
+  }
+}
+
+void jb::yaml::merge(
+    class_overrides& by_class, YAML::Node node) {
+  // Only Map nodes can override by-class values ...
+  if (not node.IsMap()) {
+    return;
+  }
+  // ... iterate over the node, searching for nodes with a key starting
+  // with ':' ...
+  for (auto i : node) {
+    // ... the node is a map, there should be keys for all sub nodes ...
+    JB_ASSERT_THROW(i.first);
+    // ... found a key, check the format ...
+    std::string key = i.first.as<std::string>();
+    if (key.find(":") != 0) {
+      continue;
+    }
+    // ... try to insert into the map ...
+    auto ins = by_class.emplace(key, i.second);
+    if (ins.second == true) {
+      // ... good insert, nothing left to do ...
+      continue;
+    }
+    // ... okay there was a node for the class in the map already,
+    // need to merge the values ...
+    merge_node(ins.first->second, i.second);
+  }
+}
+
+jb::class_overrides jb::yaml::clone(class_overrides const& by_class) {
+  class_overrides tmp;
+  for (auto const& i : by_class) {
+    tmp.emplace(i.first, YAML::Clone(i.second));
+  }
+  return tmp;
+}
+
---
+++ jb/ut_merge_yaml.cpp
@@ -0,0 +1,94 @@
+#include <jb/merge_yaml.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <string>
+
+/**
+ * @test Verify merging of two maps.
+ */
+BOOST_AUTO_TEST_CASE(merge_yaml_map) {
+  YAML::Node source = YAML::Load("{a: 1, b: 2}");
+  YAML::Node target = YAML::Load("{a: 3, c: 4}");
+  jb::yaml::merge_node(target, source);
+
+  BOOST_CHECK_EQUAL(target["a"].as<std::string>(), "1");
+  BOOST_CHECK_EQUAL(target["b"].as<std::string>(), "2");
+  BOOST_CHECK_EQUAL(target["c"].as<std::string>(), "4");
+}
+
+/**
+ * @test Verify merging of two sequences.
+ */
+BOOST_AUTO_TEST_CASE(merge_yaml_sequences) {
+  YAML::Node source = YAML::Load("[11, 22, 33, 44]");
+  YAML::Node ss = YAML::Load("[0, 1]");
+  jb::yaml::merge_node(ss, source);
+
+  auto actual = ss.as<std::vector<int>>();
+  std::vector<int> expected({11, 22, 33, 44});
+  BOOST_CHECK_EQUAL_COLLECTIONS(
+      actual.begin(), actual.end(), expected.begin(), expected.end());
+
+  YAML::Node ls = YAML::Load("[0, 1, 2, 3, 4, 5]");
+  jb::yaml::merge_node(ls, source);
+
+  actual = ls.as<std::vector<int>>();
+  expected.assign({11, 22, 33, 44, 4, 5});
+  BOOST_CHECK_EQUAL_COLLECTIONS(
+      actual.begin(), actual.end(), expected.begin(), expected.end());
+}
+
+/**
+ * @test Verify merging of sequences of maps
+ */
+BOOST_AUTO_TEST_CASE(merge_yaml_sequences_of_maps) {
+  YAML::Node source = YAML::Load("[{a: 1}, {b: 2}, {a: 3, b: 3}, {c: 4}]");
+  YAML::Node target = YAML::Load("[{}, {a: 11}, {c: 3}]");
+  jb::yaml::merge_node(target, source);
+
+  BOOST_REQUIRE_EQUAL(target.size(), 4);
+  BOOST_CHECK_EQUAL(target[0]["a"].as<int>(), 1);
+  BOOST_CHECK(not target[0]["b"].IsDefined());
+
+  BOOST_CHECK_EQUAL(target[1]["a"].as<int>(), 11);
+  BOOST_CHECK_EQUAL(target[1]["b"].as<int>(), 2);
+
+  BOOST_CHECK_EQUAL(target[2]["a"].as<int>(), 3);
+  BOOST_CHECK_EQUAL(target[2]["b"].as<int>(), 3);
+  BOOST_CHECK_EQUAL(target[2]["c"].as<int>(), 3);
+
+  BOOST_CHECK(not target[3]["a"].IsDefined());
+  BOOST_CHECK_EQUAL(target[3]["c"].as<int>(), 4);
+}
+
+/**
+ * @test Verify merging of two nested maps.
+ */
+BOOST_AUTO_TEST_CASE(merge_yaml_nested) {
+  YAML::Node source = YAML::Load("{l0: {a: 1, b: 2}, l1: [1,2,3]}");
+  YAML::Node target = YAML::Load("{l0: {a: 3, c: 4}, l2: [0,0]}");
+  jb::yaml::merge_node(target, source);
+
+  BOOST_CHECK_EQUAL(target["l0"]["a"].as<std::string>(), "1");
+  BOOST_CHECK_EQUAL(target["l0"]["b"].as<std::string>(), "2");
+  BOOST_CHECK_EQUAL(target["l0"]["c"].as<std::string>(), "4");
+  BOOST_CHECK_EQUAL(target["l1"][0].as<int>(), 1);
+  BOOST_CHECK_EQUAL(target["l1"][1].as<int>(), 2);
+  BOOST_CHECK_EQUAL(target["l1"][2].as<int>(), 3);
+}
+
+/**
+ * @test Verify merging of invalid nodes throws the right thing.
+ */
+BOOST_AUTO_TEST_CASE(merge_yaml_invalid_source) {
+  YAML::Node target = YAML::Load("{a: 3, c: 4}");
+
+  BOOST_CHECK_THROW(
+      jb::yaml::merge_node(target, YAML::Node()),
+      std::runtime_error);
+
+  BOOST_CHECK_THROW(
+      jb::yaml::merge_node(target, YAML::Load("{a: 1}")["b"]),
+      std::runtime_error);
+}
+
