4d47bd32bb993508772f94ef8945ea8a1fa43738
Carlos O'Ryan <coryan@users.noreply.github.com>
2016-07-03 19:10:38 -0400

    Start adapting Travis CI scripts for trusty

--- ci/install.sh
+++ ci/install.sh
@@ -27,53 +27,38 @@ else
     exit 1
 fi
 
-# ... install all the boost libraries ...
-sudo apt-get -qq -y install boost1.55
-
-# ... install the FFT3W libraries ...
-sudo apt-get -qq -y install libfftw3-dev
-
-# ... install a recent version of autoconf, automake and whatever make
-# comes through that long list of apt sources.  We do not install
-# autoconf because we will need to manually install it ...
-sudo apt-get -qq -y install autoconf automake make
+# ... install all the dependencies ...
+sudo apt-get -qq -y install \
+    autoconf-archive \
+    automake \
+    g++ \
+    gcc \
+    git \
+    libboost1.55-all-dev \
+    libfftw3-dev \
+    libyaml-cpp-dev \
+    make \
+    wget
 
 # ... we will only need doxygen if we are going to upload the documents ...
 if [ "x${GENDOCS}" == "xyes" ]; then
     sudo apt-get -qq -y install doxygen
 fi
 
-# ... manually download the a sufficiently recent autoconf-archive
-# extract it, compile it and install it in the /usr prefix ...
-wget -q http://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2015.02.24.tar.xz
-tar -xf autoconf-archive-2015.02.24.tar.xz
-(cd autoconf-archive-2015.02.24 && ./configure --prefix=/usr && make && sudo make install)
-
-# ... we are going to need cmain to compile yaml-cpp ...
-sudo apt-get -qq -y install cmake
-
-# ... manually download the version of yaml-cpp that works with
-# JayBeams, extract it, compile it, and install it in /usr ...
-wget -q https://github.com/jbeder/yaml-cpp/archive/release-0.5.1.tar.gz
-tar -xf release-0.5.1.tar.gz
-(cd yaml-cpp-release-0.5.1 && \
-    mkdir build && cd build && \
-    cmake -DCMAKE_INSTALL_PREFIX=/usr .. && \
-    make && make test && sudo make install)
-
 # ... manually download Skye (one of my own libraries), extract it,
 # compile it (it is a header-only library, but the tests are executed
 # this way), and install it in /usr.  Notice how we set the CXX
 # environment variable ...
-wget -q https://github.com/coryan/Skye/releases/download/v0.2/skye-0.2.tar.gz
-tar -xf skye-0.2.tar.gz
+wget -q https://github.com/coryan/Skye/releases/download/v0.3/skye-0.3.tar.gz
+tar -xf skye-0.3.tar.gz
 (source ${TRAVIS_BUILD_DIR?}/ci/before_script.sh && \
-    cd skye-0.2 && ./configure && make check && sudo make install)
+        cd skye-0.3 && \
+        ./configure --with-boost-libdir=/usr/lib/x86_64-linux-gnu && \
+        make check && \
+        sudo make install)
 
 # ... manually download a recent version of lcov from a debian source
 # repository, extract it, compile it and install it ...
-wget -q http://http.debian.net/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz
-tar xf lcov_1.11.orig.tar.gz
-sudo make -C lcov-1.11/ install
+sudo apt-get -qq -y install lcov
 
 exit 0
