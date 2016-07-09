bba33f06bb63f0b85b5a0d62ee0b473512e5a514
Carlos O'Ryan <coryan@users.noreply.github.com>
2016-07-09 18:22:08 -0400

    The script needs to work with shells that do not set UID

--- ci/create-build-image.sh
+++ ci/create-build-image.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# On the Travis server, /bin/sh does not define UID, so we define it.
+if [ "x$UID"  = "x" ]; then
+    UID=$(id -u)
+fi
+
 docker build -q - <<__EOF__
-FROM coryan/${IMAGE?}
+FROM ${IMAGE?}
 ARG user
 ARG uid
 
