95e245339636c7313908a65f6279f50e913a0f33
Carlos O'Ryan <coryan@users.noreply.github.com>
2016-07-09 18:32:32 -0400

    Use a VOLUME directive change the working directory to a more convenient place

--- docker/dev/runvariant.sh
+++ docker/dev/runvariant.sh
@@ -29,9 +29,10 @@ ARG uid
 
 WORKDIR /
 RUN useradd -m -u \$uid \$user
+VOLUME /home/\$user/jaybeams
 
 USER \$user
-WORKDIR /home/\$user
+WORKDIR /home/\$user/jaybeams
 __EOF__
 ) | docker build -q --build-arg user=$USER --build-arg uid=$UID -)
 
