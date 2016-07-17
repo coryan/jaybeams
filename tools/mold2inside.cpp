10e2e91f0e08b9b8ddc4d25bc2b99218412ae47a
Carlos O'Ryan <coryan@users.noreply.github.com>
2016-07-16 20:44:38 -0400

    Use std::function<> to parse MoldUDP64 packets and break them down in their component ITCH-5.x messages.

--- tools/mold2inside.cpp
+++ tools/mold2inside.cpp
@@ -34,7 +34,12 @@ class config : public jb::config_object {
 
 class mold_channel {
  public:
+  typedef std::function<void(
+      std::chrono::steady_clock::time_point, std::uint64_t, std::size_t,
+      char const*, std::size_t)> buffer_handler;
+  
   mold_channel(
+      buffer_handler handler,
       boost::asio::io_service& io,
       std::string const& listen_address,
       int multicast_port,
@@ -46,8 +51,10 @@ class mold_channel {
       boost::system::error_code const& ec, size_t bytes_received);
 
  private:
+  buffer_handler handler_;
   boost::asio::ip::udp::socket socket_;
   std::uint64_t expected_sequence_number_;
+  std::size_t message_offset_;
 
   static std::size_t const buflen = 1<<16;
   char buffer_[buflen];
@@ -55,6 +62,27 @@ class mold_channel {
 };
 
 } // anonymous namespace
+
+#define KNOWN_ITCH5_MESSAGES                    \
+  jb::itch5::add_order_message,                 \
+  jb::itch5::add_order_mpid_message, \
+  jb::itch5::broken_trade_message, \
+  jb::itch5::cross_trade_message, \
+  jb::itch5::ipo_quoting_period_update_message, \
+  jb::itch5::market_participant_position_message, \
+  jb::itch5::mwcb_breach_message, \
+  jb::itch5::mwcb_decline_level_message, \
+  jb::itch5::net_order_imbalance_indicator_message, \
+  jb::itch5::order_cancel_message, \
+  jb::itch5::order_delete_message, \
+  jb::itch5::order_executed_message, \
+  jb::itch5::order_executed_price_message, \
+  jb::itch5::order_replace_message, \
+  jb::itch5::reg_sho_restriction_message, \
+  jb::itch5::stock_directory_message, \
+  jb::itch5::stock_trading_action_message, \
+  jb::itch5::system_event_message, \
+  jb::itch5::trade_message
 
 int main(int argc, char* argv[]) try {
   config cfg;
@@ -64,20 +92,13 @@ int main(int argc, char* argv[]) try {
 
   boost::asio::io_service io_service;
 
-  mold_channel channel(
-      io_service, cfg.listen_address(), cfg.multicast_port(),
-      cfg.multicast_group());
-
-  io_service.run();
-
-#if 0
   boost::iostreams::filtering_ostream out;
   jb::open_output_file(out, cfg.output_file());
 
   std::map<jb::itch5::stock_t, jb::offline_feed_statistics> per_symbol;
   jb::offline_feed_statistics stats(cfg.stats());
 
-  auto cb = [&](
+  auto cb = [&stats, &cfg, &out, &per_symbol](
       jb::itch5::compute_inside::time_point recv_ts,
       jb::itch5::message_header const& header,
       jb::itch5::stock_t const& stock,
@@ -107,14 +128,27 @@ int main(int argc, char* argv[]) try {
   };
 
   jb::itch5::compute_inside handler(cb);
-  jb::itch5::process_iostream(in, handler);
+  auto process_buffer = [&handler](
+      std::chrono::steady_clock::time_point recv_ts,
+      std::uint64_t msgcnt, std::size_t msgoffset,
+      char const* msgbuf, std::size_t msglen) {
+    jb::itch5::process_buffer_mlist<
+          jb::itch5::compute_inside,KNOWN_ITCH5_MESSAGES>::process(
+        handler, recv_ts, msgcnt, msgoffset, msgbuf, msglen);
+  };
+
+  mold_channel channel(
+      process_buffer, io_service, cfg.listen_address(), cfg.multicast_port(),
+      cfg.multicast_group());
+
+  io_service.run();
 
   jb::offline_feed_statistics::print_csv_header(std::cout);
   for (auto const& i : per_symbol) {
     i.second.print_csv(i.first.c_str(), std::cout);
   }
   stats.print_csv("__aggregate__", std::cout);
-#endif
+
 
   return 0;
 } catch(jb::usage const& u) {
@@ -147,7 +181,7 @@ std::string default_listen_address() {
 }
 
 std::string default_multicast_group() {
-  return "FF01::1";
+  return "ffa1::aa";
 }
 
 int default_multicast_port() {
@@ -190,25 +224,31 @@ void config::validate() const {
 }
 
 mold_channel::mold_channel(
+    buffer_handler handler,
     boost::asio::io_service& io,
     std::string const& listen_address,
     int multicast_port,
     std::string const& multicast_group)
-    : socket_(io)
-    , expected_sequence_number_(0) {
+    : handler_(handler)
+    , socket_(io)
+    , expected_sequence_number_(0)
+    , message_offset_(0) {
   auto address = boost::asio::ip::address::from_string(listen_address);
   boost::asio::ip::udp::endpoint endpoint(address, multicast_port);
   boost::asio::ip::udp::socket socket(io);
   socket_.open(endpoint.protocol());
   socket_.set_option(boost::asio::ip::udp::socket::reuse_address(true));
-  socket_.bind(endpoint);
 
+  JB_LOG(info) << "Requested endpoint=" << endpoint;
   auto group_address = boost::asio::ip::address::from_string(multicast_group);
-  socket_.set_option(boost::asio::ip::multicast::join_group(group_address));
-
-  JB_LOG(info) << "Listening on endpoint=" << socket_.local_endpoint()
-               << ", requested=" << endpoint
-               << " for data in multicast group=" << group_address;
+  if (group_address.is_multicast()) {
+    socket_.set_option(boost::asio::ip::multicast::join_group(group_address));
+    socket_.set_option(boost::asio::ip::multicast::enable_loopback(true));
+    JB_LOG(info) << " .. joining multicast group=" << group_address;
+  } else {
+    socket_.bind(endpoint);
+    JB_LOG(info) << " .. bound to endpoint=" << socket_.local_endpoint();
+  }
 
   restart_async_receive_from();
 }
@@ -224,20 +264,55 @@ void mold_channel::restart_async_receive_from() {
 void mold_channel::handle_received(
     boost::system::error_code const& ec, size_t bytes_received) {
   if (!ec and bytes_received > 0) {
+    // Fetch the current timestamp, all the messages in the
+    // MoldUDP64 packet share the same timestamp ...
+    auto recv_ts = std::chrono::steady_clock::now();
+    // ... fetch the sequence number of the first message in the
+    // MoldUDP64 packet ...
     auto sequence_number = jb::itch5::decoder<true,std::uint64_t>::r(
         bytes_received, buffer_,
         jb::itch5::mold_udp_protocol::sequence_number_offset);
-
+    // ... and fetch the number of blocks in the MoldUDP64 packet ...
     auto block_count = jb::itch5::decoder<true,std::uint16_t>::r(
         bytes_received, buffer_,
         jb::itch5::mold_udp_protocol::block_count_offset);
 
+    JB_LOG(info) << "Received packet with starting seqno=" << sequence_number;
+    // ... if the message is out of order we simply print the problem,
+    // in a more realistic application we would need to reorder them
+    // and gap fill if needed, and sometimes do even more complicated
+    // things ...
     if (sequence_number != expected_sequence_number_) {
       JB_LOG(info) << "Mismatched sequence number, expected="
                    << expected_sequence_number_
                    << ", got=" << sequence_number;
     }
-    expected_sequence_number_ += sequence_number + block_count;
+
+    
+
+    //  offset represents the start of the MoldUDP64 block ...
+    std::size_t offset = jb::itch5::mold_udp_protocol::block_count_offset + 2;
+    // ... process each message in the MoldUDP64 packet, in order.
+    for (std::size_t block = 0; block != block_count; ++block) {
+      // ... fetch the block size ...
+      auto message_size = jb::itch5::decoder<true,std::uint16_t>::r(
+          bytes_received, buffer_, offset);
+      // ... increment the offset into the MoldUDP64 packet, this is
+      // the start of the ITCH-5.x message ...
+      offset += 2;
+      // ... process the buffer ...
+      handler_(
+          recv_ts, expected_sequence_number_, message_offset_,
+          buffer_ + offset, message_size);
+
+      // ... increment counters to reflect that this message was
+      // proceesed ...
+      sequence_number++;
+      message_offset_ += message_size;
+    }
+    // ... since we are not dealing with gaps, or message reordering
+    // just reset the next expected number ...
+    expected_sequence_number_ = sequence_number;
   }
   restart_async_receive_from();
 }
