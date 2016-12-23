2b6be93269101b2e762094d8f9b21cdcca1a9581
GFariasR <gfariasr@noreply.gitgub.com>
2016-12-23 17:49:05 -0500

    add convenient static to get the limit prices

--- jb/itch5/quote_defaults.hpp
+++ jb/itch5/quote_defaults.hpp
@@ -9,14 +9,24 @@ namespace itch5 {
 /// A simple representation for price + quantity
 using half_quote = std::pair<price4_t, int>;
 
+/// convenient value to represent an empty bid limit price
+static price4_t empty_bid_price() {
+  return price4_t(0);
+}
+
+/// convenient value to represent an empty offer limit price
+static price4_t empty_offer_price() {
+  return max_price_field_value<price4_t>();
+}
+
 /// The value used to represent an empty bid
 static half_quote empty_bid() {
-  return half_quote(price4_t(0), 0);
+  return half_quote(empty_bid_price(), 0);
 }
 
 /// The value used to represent an empty offer
 static half_quote empty_offer() {
-  return half_quote(max_price_field_value<price4_t>(), 0);
+  return half_quote(empty_offer_price(), 0);
 }
 
 } // namespace itch5
