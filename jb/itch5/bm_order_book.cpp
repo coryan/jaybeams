a2b1fc534ec2e041e7b7e6507914f5925350b964
Carlos O'Ryan <coryan@users.noreply.github.com>
2016-12-25 15:21:34 -0500

    Add histogram to compute book building latency.

--- jb/itch5/bm_order_book.cpp
+++ jb/itch5/bm_order_book.cpp
@@ -12,6 +12,8 @@
 #include <jb/itch5/map_based_order_book.hpp>
 #include <jb/itch5/process_iostream.hpp>
 #include <jb/fileio.hpp>
+#include <jb/histogram.hpp>
+#include <jb/integer_range_binning.hpp>
 #include <jb/log.hpp>
 
 #include <stdexcept>
@@ -40,6 +42,7 @@ public:
   jb::config_attribute<config, array_config> array_book;
   jb::config_attribute<config, map_config> map_book;
   jb::config_attribute<config, int> stop_after_seconds;
+  jb::config_attribute<config, int> max_processing_latency_nanoseconds;
 };
 
 /// A simple struct to signal termination of the
@@ -59,23 +62,38 @@ struct abort_process_iostream {};
  */
 template <typename book_type, typename book_type_config>
 void run_inside(config const& cfg, book_type_config const& book_cfg) {
+  // initialize the logging framework ...
   jb::log::init(cfg.log());
 
+  // ... setup the input file, automatically determines if
+  // uncompression is necessary based on the filename ...
   boost::iostreams::filtering_istream in;
   jb::open_input_file(in, cfg.input_file());
 
+  // ... create a histogram to capture latency statistics, the main
+  // point of the benchmark after all ...
+  using latency_histogram =
+      jb::histogram<jb::integer_range_binning<std::int64_t>>;
+  latency_histogram histo(
+      latency_histogram::binning_strategy(
+          0, cfg.max_processing_latency_nanoseconds()));
+
+  // ... convert the configuration parameter into a duration type ...
   std::chrono::seconds stop_after(cfg.stop_after_seconds());
 
+  // ... create a callback of the right type ...
   using callback_type =
       typename jb::itch5::compute_book<book_type>::callback_type;
-  callback_type cb = [stop_after](
+  callback_type cb = [&histo, stop_after](
       jb::itch5::message_header const& header,
       jb::itch5::order_book<book_type> const& updated_book,
       jb::itch5::book_update const& update) {
-    if (stop_after != std::chrono::seconds(0) and
-        stop_after <= header.timestamp.ts) {
+    using namespace std::chrono;
+    if (stop_after != seconds(0) and stop_after <= header.timestamp.ts) {
       throw abort_process_iostream{};
     }
+    auto pl = steady_clock::now() - update.recvts;
+    histo.sample(duration_cast<nanoseconds>(pl).count());
   };
 
   jb::itch5::compute_book<book_type> handler(cb, book_cfg);
@@ -87,6 +105,7 @@ void run_inside(config const& cfg, book_type_config const& book_cfg) {
     JB_LOG(info) << "process_iostream aborted, stop_after_seconds="
                  << cfg.stop_after_seconds();
   }
+  JB_LOG(info) << "processing latency (ns): " << histo.summary();
 }
 
 int main(int argc, char* argv[]) try {
@@ -120,8 +139,14 @@ namespace defaults {
 #define JB_ITCH5_DEFAULT_bm_order_book_stop_after_seconds (9 * 60 + 35) * 60
 #endif // JB_ITCH5_DEFAULT_bm_order_book_stop_after_seconds
 
+#ifndef JB_ITCH5_DEFAULT_bm_order_book_max_processing_latency
+#define JB_ITCH5_DEFAULT_bm_order_book_max_processing_latency 1000000
+#endif // JB_ITCH5_DEFAULT_bm_order_book_max_processing_latency
+
 int constexpr stop_after_seconds =
     JB_ITCH5_DEFAULT_bm_order_book_stop_after_seconds;
+int constexpr max_processing_latency_nanoseconds =
+    JB_ITCH5_DEFAULT_bm_order_book_max_processing_latency;
 } // namespace defaults
 
 config::config()
@@ -144,7 +169,15 @@ config::config()
                   "seconds in the input.  For example, if set to 34500 (= 9 * "
                   "3600 + 35 * 60) the processing will stop when the first "
                   "event timestamped after 09:35:00 is received."),
-          this, defaults::stop_after_seconds) {
+          this, defaults::stop_after_seconds)
+    , max_processing_latency_nanoseconds(
+          desc("max-processing-time-nanoseconds")
+              .help(
+                  "Configure the processing latency histogram to expect"
+                  " no more that no processing time is higher than this value."
+                  "   Higher values consume more memory, but give more accurate"
+                  " results for high percentiles."),
+          this, defaults::max_processing_latency_nanoseconds) {
 }
 
 void config::validate() const {
@@ -156,6 +189,9 @@ void config::validate() const {
   }
   if (stop_after_seconds() < 0) {
     throw jb::usage("The stop-after-seconds must be >= 0", 1);
+  }
+  if (max_processing_latency_nanoseconds() < 0) {
+    throw jb::usage("The max-processing-latency-nanoseconds must be >= 0", 1);
   }
 
   log().validate();
