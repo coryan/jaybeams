f72084e7c061b9a0b9cf1683d9067c3a224d9d36
GFariasR <gfariasr@noreply.gitgub.com>
2016-12-25 12:29:49 -0500

    move iostream usage to cpp file

--- jb/itch5/array_based_order_book.cpp
+++ jb/itch5/array_based_order_book.cpp
@@ -1,5 +1,7 @@
 #include <jb/itch5/array_based_order_book.hpp>
 
+#include <sstream>
+
 namespace jb {
 namespace itch5 {
 
@@ -7,7 +9,7 @@ namespace itch5 {
 void validate_add_order_params(int qty, price4_t px) {
   if (qty <= 0 or px >= max_price_field_value<price4_t>()) {
     std::ostringstream os;
-    os << "array_based_order_book::validate_add_order_params out of range."
+    os << "array_based_book_side::validate_add_order_params out of range."
        << " px=" << px << " qty=" << qty;
     throw jb::feed_error(os.str());
   }
@@ -21,6 +23,25 @@ void array_based_order_book::config::validate() const {
        << ", value=" << max_size();
     throw jb::usage(os.str(), 1);
   }
+}
+
+/// handle exception with tk_begin_top_, px, and qty
+void handle_exception(
+    std::string str, std::size_t tk_begin_top, price4_t px, int qty) {
+  std::ostringstream os;
+  os << str << " tk_begin_top_=" << tk_begin_top << " px=" << px
+     << " qty=" << qty;
+  throw jb::feed_error(os.str());
+}
+
+/// handle exception with relative inside and px relative position
+void handle_exception(
+    std::string str, std::size_t tk_begin_top, std::size_t tk_inside,
+    std::size_t rel_px, price4_t px, int qty) {
+  std::ostringstream os;
+  os << str << " tk_begin_top_=" << tk_begin_top << " tk_inside_" << tk_inside
+     << " px relative position " << rel_px << " px=" << px << " qty=" << qty;
+  throw jb::feed_error(os.str());
 }
 } // namespace itch5
 } // namespace jb
--- jb/itch5/array_based_order_book.hpp
+++ jb/itch5/array_based_order_book.hpp
@@ -13,7 +13,6 @@
 #include <cmath>
 #include <functional>
 #include <map>
-#include <sstream>
 #include <utility>
 #include <vector>
 
@@ -22,6 +21,9 @@ namespace itch5 {
 
 // forward declaration of
 void validate_add_order_params(int, price4_t px = price4_t(0));
+void handle_exception(std::string, std::size_t, price4_t, int);
+void handle_exception(
+    std::string, std::size_t, std::size_t, std::size_t, price4_t, int);
 
 namespace defaults {
 
@@ -223,12 +225,10 @@ public:
     if (side<compare_t>::better_level(tk_begin_top_, tk_px)) {
       auto price_it = bottom_levels_.find(tk_px);
       if (price_it == bottom_levels_.end()) {
-        std::ostringstream os;
-        os << "array_based_order_book::reduce_order."
-           << " Trying to reduce a non-existing bottom_levels_ price."
-           << " tk_begin_top_=" << tk_begin_top_ << " px=" << px
-           << " qty=" << qty;
-        throw jb::feed_error(os.str());
+        handle_exception(
+            "array_based_book_side::reduce_order."
+            " Trying to reduce non-existing bottom_levels_price.",
+            tk_begin_top_, px, qty);
       }
       // ... reduce the quantity ...
       price_it->second -= qty;
@@ -246,25 +246,20 @@ public:
 
     // handles the top_levels_ price
     if (side<compare_t>::better_level(tk_px, tk_inside_)) {
-      std::ostringstream os;
-      os << "array_based_order_book::reduce_order."
-         << " Trying to reduce a non-existing top_levels_ price"
-         << " (better px_inside)."
-         << " tk_begin_top_=" << tk_begin_top_ << " tk_inside_=" << tk_inside_
-         << " tk_end_top_=" << tk_end_top_ << " px=" << px << " qty=" << qty;
-      throw jb::feed_error(os.str());
+      handle_exception(
+          "array_based_book_side::reduce_order."
+          " Trying to reduce a non-existing top_levels_ price"
+          " (better px_inside).",
+          tk_begin_top_, px, qty);
     }
     // get px relative position
     auto rel_px = side<compare_t>::level_to_relative(tk_begin_top_, tk_px);
     if (top_levels_.at(rel_px) == 0) {
-      std::ostringstream os;
-      os << "array_based_order_book::reduce_order."
-         << " Trying to reduce a non-existing top_levels_ price"
-         << " (top_levels_[rel_px] == 0)."
-         << " tk_begin_top_=" << tk_begin_top_ << " tk_inside_=" << tk_inside_
-         << " tk_end_top_=" << tk_end_top_ << " px relative position=" << rel_px
-         << " px=" << px << " qty=" << qty;
-      throw jb::feed_error(os.str());
+      handle_exception(
+          "array_based_book_side::reduce_order."
+          " Trying to reduce a non-existing top_levels_ price"
+          " (top_levels_[rel_px] == 0).",
+          tk_begin_top_, tk_inside_, rel_px, px, qty);
     }
 
     // ... reduce the quantity ...
