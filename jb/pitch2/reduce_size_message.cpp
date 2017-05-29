17e03192ca7d5b3f16ef5ac0b1d9a39eaf29264a
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-05-28 21:55:53 -0400

    Refactor streaming operators for jb::pitch2::reduce_size*

--- jb/pitch2/reduce_size_message.cpp
+++ jb/pitch2/reduce_size_message.cpp
@@ -5,7 +5,9 @@
 namespace jb {
 namespace pitch2 {
 
-std::ostream& operator<<(std::ostream& os, reduce_size_long_message const& x) {
+template <typename quantity_t>
+std::ostream& stream_reduce_size_message(
+    std::ostream& os, reduce_size_message<quantity_t> const& x) {
   return os << "length=" << static_cast<int>(x.length.value())
             << ",message_type=" << static_cast<int>(x.message_type.value())
             << ",time_offset=" << x.time_offset.value()
@@ -13,12 +15,12 @@ std::ostream& operator<<(std::ostream& os, reduce_size_long_message const& x) {
             << ",canceled_quantity=" << x.canceled_quantity.value();
 }
 
+std::ostream& operator<<(std::ostream& os, reduce_size_long_message const& x) {
+  return stream_reduce_size_message(os, x);
+}
+
 std::ostream& operator<<(std::ostream& os, reduce_size_short_message const& x) {
-  return os << "length=" << static_cast<int>(x.length.value())
-            << ",message_type=" << static_cast<int>(x.message_type.value())
-            << ",time_offset=" << x.time_offset.value()
-            << ",order_id=" << x.order_id.value()
-            << ",canceled_quantity=" << x.canceled_quantity.value();
+  return stream_reduce_size_message(os, x);
 }
 
 } // namespace pitch2
