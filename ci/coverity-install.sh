4f6dcd6fe275de28e066712dba71933f1fc80429
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-05-31 22:24:25 -0400

    Log more when installing Coverity Scan.

--- ci/coverity-install.sh
+++ ci/coverity-install.sh
@@ -13,15 +13,16 @@ wget -q https://scan.coverity.com/download/linux64 \
 test -r coverity_tool.current.md5 || touch coverity_tool.current.md5
 
 if cmp -s coverity_tool.md5 coverity_tool.current.md5; then
-    :
+    echo "Coverity Scan is up to date, no download required."
 else
+    echo "Downloading Coverity Scan..."
     wget -q https://scan.coverity.com/download/linux64 \
          --post-data "token=${COVERITY_TOKEN?}&project=jaybeams2" \
-         -O coverity_tool.tgz
-    cp  coverity_tool.md5 coverity_tool.current.md5
+         -O coverity_tool.tgz && \
+        tar xf coverity_tool.tgz && \
+        cp  coverity_tool.md5 coverity_tool.current.md5
+    echo "   done."
 fi
-
-tar xf coverity_tool.tgz
 
 exit 0
 
