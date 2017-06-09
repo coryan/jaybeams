e08110962d65d6391684aa835fbf8d2abdaf0d46
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-06-08 22:31:41 -0400

    Improve coverage in jb::ehs components.

--- jb/ehs/ut_acceptor.cpp
+++ jb/ehs/ut_acceptor.cpp
@@ -38,6 +38,8 @@ BOOST_AUTO_TEST_CASE(acceptor) {
   boost::asio::ip::tcp::socket sock{io};
   sock.connect(listen);
   // ... prepare and send the request ...
+  using jb::ehs::request_type;
+  using jb::ehs::response_type;
   request_type req;
   req.method = "GET";
   req.url = "/";
@@ -46,7 +48,8 @@ BOOST_AUTO_TEST_CASE(acceptor) {
   req.fields.replace("user-agent", "acceptor_base");
   beast::http::prepare(req);
   beast::http::write(sock, req);
-  // ... receive the replay ...
+
+  // ... receive the reply ...
   beast::streambuf sb;
   response_type res;
   beast::http::read(sock, sb, res);
@@ -56,9 +59,128 @@ BOOST_AUTO_TEST_CASE(acceptor) {
   BOOST_CHECK_EQUAL(res.fields["server"], "test");
   BOOST_CHECK_EQUAL(res.body, "OK\n");
 
+  // ... closing the socket triggers more behaviors in the acceptor
+  // and connector classes ...
+  sock.close();
+
+  // ... shutdown the acceptor and stop the io_service ...
   io_service.dispatch([&acceptor, &io_service] {
     acceptor.shutdown();
     io_service.stop();
   });
+  // ... wait for the acceptor thread ...
   t.join();
+}
+
+/**
+ * @test Verify that jb::ehs::acceptor accepts multiple connections.
+ */
+BOOST_AUTO_TEST_CASE(acceptor_multiple_connections) {
+  // Let the operating system pick a listening address ...
+  boost::asio::ip::tcp::endpoint ep{boost::asio::ip::address_v4(), 0};
+
+  // ... create a dispatcher and a handler for / ...
+  auto dispatcher = std::make_shared<jb::ehs::request_dispatcher>("test");
+  // ... create a IO service, and an acceptor in the default address ...
+  boost::asio::io_service io_service;
+  jb::ehs::acceptor acceptor(io_service, ep, dispatcher);
+
+  // ... get the local listening endpoint ...
+  auto listen = acceptor.local_endpoint();
+
+  // ... create a separate io service for the client side ...
+  boost::asio::io_service io;
+  // ... open a socket to send the HTTP request ...
+  boost::asio::ip::tcp::socket sock{io};
+  sock.connect(listen);
+  sock.close();
+
+  sock.connect(listen);
+  sock.close();
+}
+
+/**
+ * @test Verify that jb::ehs::connection can handle multiple requests.
+ */
+BOOST_AUTO_TEST_CASE(connection_multiple_requests) {
+  // Let the operating system pick a listening address ...
+  boost::asio::ip::tcp::endpoint ep{boost::asio::ip::address_v4(), 0};
+
+  // ... create a dispatcher and a handler for / ...
+  auto dispatcher = std::make_shared<jb::ehs::request_dispatcher>("test");
+  // ... create a IO service, and an acceptor in the default address ...
+  boost::asio::io_service io_service;
+  jb::ehs::acceptor acceptor(io_service, ep, dispatcher);
+
+  // ... run a separate thread with the io_service so we can write
+  // synchronous code in the test, which is easier to follow ...
+  std::thread t([&io_service]() { io_service.run(); });
+
+  // ... get the local listening endpoint ...
+  auto listen = acceptor.local_endpoint();
+  // ... create a separate io service for the client side ...
+  boost::asio::io_service io;
+  // ... open a socket to send the HTTP request ...
+  boost::asio::ip::tcp::socket sock{io};
+  sock.connect(listen);
+
+  // ... prepare and send the request ...
+  using jb::ehs::request_type;
+  using jb::ehs::response_type;
+  request_type req;
+  req.method = "GET";
+  req.url = "/";
+  req.version = 11;
+  req.fields.replace("host", "0.0.0.0");
+  req.fields.replace("user-agent", "acceptor_base");
+  beast::http::prepare(req);
+  beast::http::write(sock, req);
+
+  // ... receive the reply ...
+  beast::streambuf sb;
+  response_type res;
+  beast::http::read(sock, sb, res);
+
+  BOOST_CHECK_EQUAL(res.status, 404);
+  BOOST_CHECK_EQUAL(res.version, 11);
+  BOOST_CHECK_EQUAL(res.fields["server"], "test");
+
+  // ... do the request+reply thing again ...
+  beast::http::prepare(req);
+  beast::http::write(sock, req);
+  beast::http::read(sock, sb, res);
+  BOOST_CHECK_EQUAL(res.status, 404);
+  BOOST_CHECK_EQUAL(res.version, 11);
+  BOOST_CHECK_EQUAL(res.fields["server"], "test");
+
+  // ... shutdown the acceptor and stop the io_service ...
+  io_service.dispatch([&acceptor, &io_service] {
+    acceptor.shutdown();
+    io_service.stop();
+  });
+  // ... wait for the acceptor thread ...
+  t.join();
+
+  // ... closing the socket triggers more behaviors in the acceptor
+  // and connector classes ...
+  sock.close();
+}
+
+/**
+ * @test Verify that jb::ehs::acceptor errors paths work.
+ */
+BOOST_AUTO_TEST_CASE(acceptor_shutdown_errors) {
+  // Let the operating system pick a listening address ...
+  boost::asio::ip::tcp::endpoint ep{boost::asio::ip::address_v4(), 0};
+
+  // ... create a dispatcher and a handler for / ...
+  auto dispatcher = std::make_shared<jb::ehs::request_dispatcher>("test");
+  // ... create a IO service, and an acceptor in the default address ...
+  boost::asio::io_service io_service;
+  jb::ehs::acceptor acceptor(io_service, ep, dispatcher);
+
+  // ... close the acceptor twice, to trigger some of the error code
+  // paths ...
+  acceptor.shutdown();
+  acceptor.shutdown();
 }
