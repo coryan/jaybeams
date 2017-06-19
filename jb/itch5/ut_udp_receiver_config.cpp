de76bb34f0babee2597f9a25b500f5fbd53addfd
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-06-19 07:31:17 -0400

    Push coverage to 100%.

--- jb/itch5/ut_udp_receiver_config.cpp
+++ jb/itch5/ut_udp_receiver_config.cpp
@@ -6,6 +6,7 @@
  * @test Verify that jb::itch5::udp_receiver_config works as expected.
  */
 BOOST_AUTO_TEST_CASE(itch5_udp_receiver_config_basic) {
+  BOOST_CHECK_NO_THROW(jb::itch5::udp_receiver_config());
   BOOST_CHECK_NO_THROW(
       jb::itch5::udp_receiver_config().address("127.0.0.1").validate());
   BOOST_CHECK_NO_THROW(
