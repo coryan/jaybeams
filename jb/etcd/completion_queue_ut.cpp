0cab95e795a781309e05c1e6202f8a72f765d66a
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-06-25 15:32:01 -0400

    Also test the case where we cancel a timer.

--- jb/etcd/completion_queue_ut.cpp
+++ jb/etcd/completion_queue_ut.cpp
@@ -11,14 +11,28 @@ BOOST_AUTO_TEST_CASE(completion_queue_basic) {
   jb::etcd::completion_queue queue;
 
   std::atomic<int> cnt(0);
+  std::atomic<int> cxl(0);
+  auto functor = [&cnt, &cxl](auto const& op, bool ok) {
+    if (not ok) {
+      ++cxl;
+    } else {
+      ++cnt;
+    }
+  };
+
+  auto canceled = queue.make_relative_timer(
+      std::chrono::milliseconds(10), "test-canceled", functor);
+  canceled->cancel();
   auto timer = queue.make_relative_timer(
-      std::chrono::milliseconds(10), [&cnt](auto op) { ++cnt; });
+      std::chrono::milliseconds(10), "test-timer", functor);
   std::thread t([&queue]() { queue.run(); });
 
   for (int i = 0; i != 100 and cnt.load() == 0; ++i) {
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
   BOOST_CHECK_EQUAL(cnt.load(), 1);
+  BOOST_CHECK_EQUAL(cxl.load(), 1);
+
   queue.shutdown();
   t.join();
 }
