d87fd57f503957ad5ebc7ac6bbf22bc2277cdc90
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-06-25 13:24:33 -0400

    Remove old async ops for reader-writer streams.

--- jb/etcd/completion_queue.hpp
+++ jb/etcd/completion_queue.hpp
@@ -1,7 +1,7 @@
 #ifndef jb_etcd_completion_queue_hpp
 #define jb_etcd_completion_queue_hpp
 
-#include <jb/etcd/async_rdwr_stream.hpp>
+#include <jb/etcd/detail/async_ops.hpp>
 
 #include <atomic>
 #include <chrono>
@@ -253,64 +253,6 @@ std::shared_ptr<detail::async_op<R>> make_async_op(Functor&& functor) {
     (void)move(tmp->callback);
   });
   return op;
-}
-
-/// Create an asynchronous write operation for the given functor
-template <typename W, typename Functor>
-std::shared_ptr<detail::async_write_op<W>> make_write_op(Functor&& f) {
-  auto op = std::make_shared<detail::async_write_op<W>>();
-  op->callback = std::move([ op, functor = std::move(f) ](bool ok) {
-    auto tmp = std::move(op);
-    functor(tmp);
-    (void)std::move(tmp->callback);
-  });
-  return op;
-}
-
-/// Create an asynchronous read operation for the given functor.
-template <typename R, typename Functor>
-std::shared_ptr<detail::async_read_op<R>> make_read_op(Functor&& f) {
-  auto op = std::make_shared<detail::async_read_op<R>>();
-  op->callback = std::move([ op, functor = std::move(f) ](bool ok) {
-    auto callback = std::move(op->callback);
-    functor(std::move(op));
-  });
-  return op;
-}
-
-/// Create an asyncrhonous WritesDone operation wrapper.
-template <typename Functor>
-std::shared_ptr<detail::writes_done_op> make_writes_done_op(Functor&& functor) {
-  auto op = std::make_shared<detail::writes_done_op>();
-  op->callback = std::move([op, functor](bool ok) {
-    auto tmp = std::move(op);
-    functor(tmp);
-    (void)std::move(tmp->callback);
-  });
-  return op;
-}
-
-/// Create an asynchronous Finish operation wrapper.
-template <typename Functor>
-std::shared_ptr<detail::finish_op> make_finish_op(Functor&& functor) {
-  auto op = std::make_shared<detail::finish_op>();
-  op->callback = std::move([op, functor](bool ok) {
-    auto callback = std::move(op->callback);
-    functor(std::move(op));
-  });
-  return op;
-}
-
-/// Create a read-write stream
-template <typename W, typename R, typename Functor>
-std::shared_ptr<async_rdwr_stream<W, R>>
-make_async_rdwr_stream(Functor&& functor) {
-  auto stream = std::make_shared<async_rdwr_stream<W, R>>();
-  stream->ready_callback = std::move([stream, functor](bool ok) {
-    auto callback = std::move(stream->ready_callback);
-    functor(stream);
-  });
-  return stream;
 }
 
 } // namespace etcd
